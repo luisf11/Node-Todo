253e65dba04739e95fd904f778f3f9c650c48c28
Ivan Alburquerque (ClaroDom) <Ivan Alburquerque@corp.codetel.com.do>
2016-09-23 16:31:16 -0400

    Added delete function to Todo model

--- features/main/TodoController.js
+++ features/main/TodoController.js
@@ -1,1 +1,66 @@
-TodoController.js
+
+var moduleTodo = angular.module('moduleTodo', []);
+
+funtion TodoController($scope, $http) {
+	
+	$scope.formData = {};
+
+ 	// when landing on the page, get all todos and show them
+	$http.get('/api/todos')
+
+		.success(function(data) {
+
+			$scope.todos = data;
+
+			console.log(data);
+		})
+
+		.error(function(data) {
+			console.log('Scope -> get() error: ' + data);
+		});
+
+
+	// when submitting the add form, send the text to the node API
+	$scope.createTodo =  function() {
+
+		$http.post('/api/todos', $scope.formData)
+
+			.success(function(data) {
+
+				$scope.formData{};
+
+				$scope.todos = data;
+
+				console.log(data);
+
+			})
+
+			.error(function(data) {
+   				console.log('Scope -> createTodo() error: ' + data);
+			});
+	};
+
+
+	// delete a todo after checking it
+	$scope.deleteTodo = function(id) {
+
+		$http.delete('/api/todos/' + id)
+			
+			.success(function(data) {
+
+				$scope.todos = data;
+
+				console.log(data);
+
+			})
+
+			.error(function(data) {
+				console.log('Scope -> deleteTodo() error: ' + data);
+			});
+	}
+
+
+
+
+
+}
