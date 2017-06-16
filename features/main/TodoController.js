fdf6c75ac6d4698b7a370f5f62ef48ed31b4a0dd
Ivan Alburquerque <AlburIvan@users.noreply.github.com>
2017-06-16 03:28:48 -0300

    Updated tab format

--- features/main/TodoController.js
+++ features/main/TodoController.js
@@ -3,54 +3,48 @@ var moduleTodo = angular.module('moduleTodo', []);
 
 function TodoController($scope, $http) {
 
-	$scope.formData = {};
+    $scope.formData = {};
 
-	// When landing on the page, get all todos and show them
-	$http.get('/api/todos')
-		.success(function(data) {
-
-			$scope.todos = data;
-			console.log(data);
-		})
-		.error(function(data) {
-			console.log('Scope -> get() error: ' + data);
-		});
-
-
-	// When submitting the add form, send the text to the node API
-	$scope.createTodo = function() {
-
-		$http.post('/api/todos', $scope.formData)
-			.success(function(data) {
-
-				$scope.formData {};
-				$scope.todos = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Scope -> createTodo() error: ' + data);
-			});
-	};
+    // When landing on the page, get all todos and show them
+    $http.get('/api/todos')
+    .success(function(data) {
+        $scope.todos = data;
+        console.log(data);
+    })
+    .error(function(data) {
+        console.log('Scope -> get() error: ' + data);
+    });
 
 
-	// delete a todo after checking it
-	$scope.deleteTodo = function(id) {
+    // When submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
 
-		$http.delete('/api/todos/' + id)
-			.success(function(data) {
+        $http.post('/api/todos', $scope.formData)
+        .success(function(data) {
 
-				$scope.todos = data;
-				console.log(data);
-
-			})
-
-		.error(function(data) {
-			console.log('Scope -> deleteTodo() error: ' + data);
-		});
-	}
-
+            $scope.formData {};
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Scope -> createTodo() error: ' + data);
+        });
+    };
 
 
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
 
+        $http.delete('/api/todos/' + id)
+        .success(function(data) {
+
+            $scope.todos = data;
+            console.log(data);
+
+        })
+        .error(function(data) {
+            console.log('Scope -> deleteTodo() error: ' + data);
+        });
+    }
 
 }
