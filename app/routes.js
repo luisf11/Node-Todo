1442851ac5ce94fdceae13949fa289ab10c93160
Ivan Alburquerque <Albur.Ivan@outlook.com>
2017-07-06 11:18:05 -0400

    Added new api routes

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,17 @@
-module.exports = (function() {
+module.exports = (function () {
     'use strict';
     var router = require('express').Router();
     var path = require('path');
 
-    router.get('/', function(req, res) {
+    var Todo = require("../core/models/todo.model");
+
+    router.get('/', function (req, res) {
         res.sendFile(path.resolve('./public/index.html'));
     });
 
-    router.get('/api/todos', function(req, res) {
+    router.get('/api/todos', function (req, res) {
 
-        Todo.find(function(err, todos) {
+        Todo.find(function (err, todos) {
 
             if (err)
                 res.send(err);
@@ -20,17 +22,17 @@ module.exports = (function() {
     });
 
 
-    router.post('/api/todos', function(req, res) {
+    router.post('/api/todos', function (req, res) {
 
         Todo.create({
             text: req.body.text,
-            done: false
-        }, function(err, todo) {
+            isCompleted: false
+        }, function (err, todo) {
 
             if (err)
                 res.send(err);
 
-            Todo.find(function(err, todos) {
+            Todo.find(function (err, todos) {
                 if (err)
                     res.send(err);
 
@@ -40,24 +42,36 @@ module.exports = (function() {
         });
     });
 
+    router.post('/api/todos/:model_id', function (req, res) {
+
+        const modelId = req.params.model_id;
+
+        Todo.findById({ _id: modelId }, function (err, todo) {
+            if (err) {
+                console.log("Error finding model...", err);
+            } else {
+                console.log("Model: " + todo);
+                todo.isCompleted = !todo.isCompleted;
+                todo.save();
+            }
+        });
+    });
 
     // get and return all the todos after you create another
-    router.delete('/api/todos/:todo_id', function(req, res) {
+    router.delete('/api/todos/delete/:todo_id', function (req, res) {
 
         Todo.remove({
             _id: req.params.todo_id
-        }, function(err, todo) {
-
+        }, function (err, todo) {
             if (err)
                 res.send(err);
 
-            Todo.find(function(err, todos) {
+            Todo.find(function (err, todos) {
                 if (err)
                     res.send(err);
 
                 res.json(todos);
             });
-
         });
 
     });
